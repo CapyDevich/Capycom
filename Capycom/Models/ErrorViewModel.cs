1df141f52a2955ebda160ef1b5788648e927e4a4
Phalaphun <zhurin.30@gmail.com>
2024-03-09 22:47:09 +1000

    Создан экземпляр шаблона проекта APS MVC

---
+++ Capycom/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Capycom.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
