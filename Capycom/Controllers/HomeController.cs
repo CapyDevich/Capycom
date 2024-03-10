c69504692ef5d510f286ec0121760bf8267eced6
Sleepy <sleepy@capybara.su>
2024-03-10 19:16:57 +1000

    Добавлена форма для входа в аккаунт

--- Capycom/Controllers/HomeController.cs
+++ Capycom/Controllers/HomeController.cs
@@ -29,6 +29,10 @@ namespace Capycom.Controllers
         {
             return View();
         }
+        public IActionResult LogIn()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
