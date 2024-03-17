2cabbc5acb760302ee59fdc1846167f17f4b849b
Phalaphun <phalaphun@capybara.su>
2024-03-17 20:39:56 +1000

    Создал тестовую вьюшку и контроллер авторизации

---
+++ Capycom/Models/UserLogInModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Capycom.Models
+{
+    public class UserLogInModel
+    {
+        [Display(Name = "Адрес электронной почты")]
+        [Required(ErrorMessage = "Некорректный адрес")]
+        [EmailAddress(ErrorMessage = "Некорректный адрес")]
+        public string CpcmUserEmail { get; set; } = null!;
+
+        [Display(Name = "Пароль")]
+        [Required(ErrorMessage = "Не указан пароль")]
+        public string CpcmUserPwd { get; set; } = null!;
+    }
+}
