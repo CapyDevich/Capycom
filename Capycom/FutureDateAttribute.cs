c17a2912da316e77a736eac7b6379fb834047cc0
Phalaphun <phalaphun@capybara.su>
2024-03-22 22:32:01 +1000

    Добавлен атрибут для проверки что дата больше сегодняшней

---
+++ Capycom/FutureDateAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Capycom
+{
+    public class FutureDateAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is DateTime dateTime && dateTime < DateTime.Now)
+            {
+                return new ValidationResult("Дата должна быть больше или равна текущей дате");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
