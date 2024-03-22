b74ccd414fd3a1b84ede5385ba91b6b3e358aef4
Phalaphun <phalaphun@capybara.su>
2024-03-22 21:21:57 +1000

    Добавлены методы по работе с постами

---
+++ Capycom/Models/UserPostModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Capycom.Models
+{
+    public class UserPostModel
+    {
+        public IFormFileCollection Files { get; set; }
+        public Guid? UserId { get; set; }
+
+        public string? Text { get; set; }
+
+        public Guid? PostFatherId { get; set; }
+
+        public DateTime? Published { get; set; }
+    }
+}
