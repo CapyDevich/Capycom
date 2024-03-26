f395e85ae0d75c960bd406df4d86d77e5595e07b
Phalaphun <zhurin.30@gmail.com>
2024-03-26 12:15:45 +1000

    Добавил возможность добавлять коммент

--- Capycom/Controllers/PostCommentController.cs
+++ Capycom/Controllers/PostCommentController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Capycom.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Hosting;
+using System.Data.Common;
+
 namespace Capycom.Controllers
 {
     public class PostCommentController : Controller
@@ -15,14 +19,86 @@ namespace Capycom.Controllers
             _config = config.Value;
             _logger = logger;
         }
-        public async Task<IActionResult> Index(Guid postId)
+        public async Task<IActionResult> ViewPost(Guid postId)
         {
             return View();
         }
-        public async Task<IActionResult> AddComment(CommentAddModel comment)
+        [Authorize]
+        public async Task<IActionResult> AddComment(CommentAddModel userComment)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                CpcmComment comment = new CpcmComment();
+                comment.CpcmCommentId = Guid.NewGuid();
+                comment.CpcmPostId = userComment.CpcmPostId;
+                comment.CpcmCommentFather = userComment.CpcmCommentFather;
+                comment.CpcmCommentText = userComment.CpcmCommentText;
+                comment.CpcmCommentCreationDate = DateTime.UtcNow;
+
+                List<string> filePaths = new List<string>();
+                List<CpcmImage> images = new List<CpcmImage>();
+
+                int i = 0;
+                foreach (var file in userComment.Files)
+                {
+                    CheckIFormFile("Files", file, 8388608, new[] { "image/jpeg", "image/png", "image/gif" });
+
+                    if (!ModelState.IsValid)
+                    {
+                        return StatusCode(400,new { message = "Неверный формат файла или превышен размер одного/нескольких файла"});
+
+                    }
+
+                    string uniqueFileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    filePaths.Add(Path.Combine("wwwroot", "uploads", uniqueFileName));
+
+                    CpcmImage image = new CpcmImage();
+                    image.CpcmImageId = Guid.NewGuid();
+                    image.CpcmCommentId = comment.CpcmCommentId;
+                    image.CpcmImagePath = filePaths.Last();
+                    image.CpcmImageOrder = 0;
+                    i++;
+
+                    images.Add(image);
+
+
+                    //Response.StatusCode = 500;
+                    //ViewData["ErrorCode"] = 500;
+                    //ViewData["Message"] = "Произошла ошибка с доступом к серверу. Если проблема сохранится спустя некоторое время, то обратитесь в техническую поддержку";
+                    //return StatusCode(500, new { message = "Произошла ошибка с доступом к серверу. Если проблема сохранится спустя некоторое время, то обратитесь в техническую поддержку" });
+
+                }
+
+                _context.AddRange(images);
+                _context.Add(comment);
+                try
+                {
+                    for (int j = 0; j < filePaths.Count; j++)
+                    {
+                        using (var fileStream = new FileStream(filePaths[j], FileMode.Create))
+                        {
+                            await userComment.Files[j].CopyToAsync(fileStream);
+                        }
+                    }
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbException)
+                {
+                    foreach (var file in filePaths)
+                    {
+                        if (System.IO.File.Exists(file))
+                        {
+                            System.IO.File.Delete(file);
+                        }
+                    }
+                    return StatusCode(500, new { message = "Произошла ошибка с доступом к серверу. Если проблема сохранится спустя некоторое время, то обратитесь в техническую поддержку" });
+                }
+                return StatusCode(200);
+            }
+            return StatusCode(400, new { message = "Текст не может быть пустым" });
+
         }
+        [Authorize]
         public async Task<IActionResult> DeleteComment(Guid postId, Guid commentId)
         {
             return View();
@@ -34,6 +110,49 @@ namespace Capycom.Controllers
         public async Task<IActionResult> GetNextComments(Guid postId, DateTime lastCommentDate)
         {
             return View();
+        }
+
+
+
+
+
+        private bool CheckIFormFileContent(IFormFile cpcmUserImage, string[] permittedTypes)//TODO: Объединить с методами при регистрации
+        {
+            if (cpcmUserImage != null && permittedTypes.Contains(cpcmUserImage.ContentType))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private bool CheckIFormFileSize(IFormFile cpcmUserImage, int size)//TODO: Объединить с методами при регистрации
+        {
+
+            if (cpcmUserImage.Length > 0 && cpcmUserImage.Length < size)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private bool CheckIFormFile(string FormFieldName, IFormFile file, int size, string[] permittedTypes)//TODO: Объединить с методами при регистрации
+        {
+            bool status = true;
+            if (!CheckIFormFileContent(file, permittedTypes))
+            {
+                ModelState.AddModelError(FormFieldName, "Допустимые типы файлов: png, jpeg, jpg, gif");
+                status = false;
+            }
+            if (!CheckIFormFileSize(file, size))
+            {
+                ModelState.AddModelError(FormFieldName, $"Максимальный размер файла: {size / 1024} Кбайт");
+                status = false;
+            }
+            return status;
         }
     }
 }
--- Capycom/Models/CommentAddModel.cs
+++ Capycom/Models/CommentAddModel.cs
@@ -1,6 +1,29 @@
-﻿namespace Capycom.Models
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Capycom.Models
 {
     public class CommentAddModel
     {
+
+        //[HiddenInput]
+        //public Guid CpcmCommentId { get; set; }
+        [Required]
+        [HiddenInput]
+        public Guid CpcmPostId { get; set; }
+
+        [Required]
+        [RequiredNonEmptyString]
+        public string? CpcmCommentText { get; set; }
+
+        [Required]
+        [HiddenInput]
+        public Guid CpcmUserId { get; set; }
+
+        [HiddenInput]
+        public Guid? CpcmCommentFather { get; set; }
+
+        [MaxFileCount(2)]
+        public IFormFileCollection? Files { get; set; }
     }
 }
