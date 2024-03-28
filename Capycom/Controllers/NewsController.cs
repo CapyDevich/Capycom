f3e3e68b8e5fcde5879f7d02d42db1854ef44b01
Phalaphun <phalaphun@capybara.su>
2024-03-28 21:51:47 +1000

    Код теперь в try catch

--- Capycom/Controllers/NewsController.cs
+++ Capycom/Controllers/NewsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using System.Data.Common;
 using System.Security.Claims;
 
 namespace Capycom.Controllers
@@ -23,107 +24,124 @@ namespace Capycom.Controllers
 		[Authorize]
 		public async Task<IActionResult> Index()
 		{
-			Guid userId = Guid.Parse(HttpContext.User.FindFirstValue("CpcmUserId"));
-			List<PostModel> postsModel = new List<PostModel>();
-
-
-			var groupIds = await _context.CpcmGroupfollowers
-				.Where(gf => gf.CpcmUserId == userId)
-				.Select(gf => gf.CpcmGroupId).ToListAsync();
-
-			// Получаем всех друзей пользователя
-			var friendIds = await _context.CpcmUserfriends
-				.Where(f => f.CmcpUserId == userId || f.CmcpFriendId == userId)
-				.Select(f => f.CmcpUserId == userId ? f.CmcpFriendId : f.CmcpUserId).ToListAsync();
-
-			// Получаем всех пользователей, на которых подписан пользователь
-			var followerIds = await _context.CpcmUserfollowers
-				.Where(f => f.CpcmUserId == userId)
-				.Select(f => f.CpcmFollowersId).ToListAsync();
-
-			// Объединяем все ID в одну коллекцию
-			var allUserIds = friendIds.Union(followerIds).ToList();
-
-			// Получаем все посты этих групп и пользователей
-			var posts = await _context.CpcmPosts
-				.Where(p => (p.CpcmGroupId.HasValue && groupIds.Contains(p.CpcmGroupId.Value)) ||
-							(p.CpcmUserId.HasValue && allUserIds.Contains(p.CpcmUserId.Value)))
-				.Include(p => p.CpcmImages)
-				//.Include(p => p.CpcmPostFatherNavigation)
-				.OrderByDescending(c => c.CpcmPostPublishedDate)
-				.Take(10)
-				.ToListAsync();
-			foreach (var post in posts)
+			try
 			{
-				if(post.CpcmPostFather != null)
+				Guid userId = Guid.Parse(HttpContext.User.FindFirstValue("CpcmUserId"));
+				List<PostModel> postsModel = new List<PostModel>();
+
+
+				var groupIds = await _context.CpcmGroupfollowers
+					.Where(gf => gf.CpcmUserId == userId)
+					.Select(gf => gf.CpcmGroupId).ToListAsync();
+
+				// Получаем всех друзей пользователя
+				var friendIds = await _context.CpcmUserfriends
+					.Where(f => f.CmcpUserId == userId || f.CmcpFriendId == userId)
+					.Select(f => f.CmcpUserId == userId ? f.CmcpFriendId : f.CmcpUserId).ToListAsync();
+
+				// Получаем всех пользователей, на которых подписан пользователь
+				var followerIds = await _context.CpcmUserfollowers
+					.Where(f => f.CpcmUserId == userId)
+					.Select(f => f.CpcmFollowersId).ToListAsync();
+
+				// Объединяем все ID в одну коллекцию
+				var allUserIds = friendIds.Union(followerIds).ToList();
+
+				// Получаем все посты этих групп и пользователей
+				var posts = await _context.CpcmPosts
+					.Where(p => (p.CpcmGroupId.HasValue && groupIds.Contains(p.CpcmGroupId.Value)) ||
+								(p.CpcmUserId.HasValue && allUserIds.Contains(p.CpcmUserId.Value)))
+					.Include(p => p.CpcmImages)
+					//.Include(p => p.CpcmPostFatherNavigation)
+					.OrderByDescending(c => c.CpcmPostPublishedDate)
+					.Take(10)
+					.ToListAsync();
+				foreach (var post in posts)
 				{
-					post.CpcmPostFatherNavigation = await GetFatherPostReccurent(post);
+					if (post.CpcmPostFather != null)
+					{
+						post.CpcmPostFatherNavigation = await GetFatherPostReccurent(post);
+					}
+					CpcmUser? userOwner = await _context.CpcmUsers.Where(u => u.CpcmUserId == post.CpcmUserId).FirstOrDefaultAsync();
+					CpcmGroup? groupOwner = await _context.CpcmGroups.Where(u => u.CpcmGroupId == post.CpcmGroupId).FirstOrDefaultAsync();
+					long likes = await _context.Database.ExecuteSqlInterpolatedAsync($@"SELECT COUNT(*) FROM CPCM_POSTLIKES WHERE CPCM_PostID = '{post.CpcmGroupId}'");
+					long reposts = await _context.Database.ExecuteSqlInterpolatedAsync($@"SELECT COUNT(*) FROM CPCM_POSTREPOSTS WHERE CPCM_PostID = '{post.CpcmGroupId}'");
+
+					PostModel postModel = new() { Post = post, UserOwner = userOwner, GroupOwner = groupOwner, LikesCount = likes, RepostsCount = reposts };
+					postsModel.Add(postModel);
 				}
-				CpcmUser? userOwner = await _context.CpcmUsers.Where(u => u.CpcmUserId == post.CpcmUserId).FirstOrDefaultAsync();
-				CpcmGroup? groupOwner = await _context.CpcmGroups.Where(u => u.CpcmGroupId == post.CpcmGroupId).FirstOrDefaultAsync();
-				long likes = await _context.Database.ExecuteSqlInterpolatedAsync($@"SELECT COUNT(*) FROM CPCM_POSTLIKES WHERE CPCM_PostID = '{post.CpcmGroupId}'");
-				long reposts = await _context.Database.ExecuteSqlInterpolatedAsync($@"SELECT COUNT(*) FROM CPCM_POSTREPOSTS WHERE CPCM_PostID = '{post.CpcmGroupId}'");
 
-				PostModel postModel = new() { Post = post, UserOwner = userOwner, GroupOwner = groupOwner, LikesCount = likes, RepostsCount = reposts};
-				postsModel.Add(postModel);
+				return View(postsModel);
 			}
-
-			return View(postsModel);
+			catch (DbException)
+			{
+				Response.StatusCode = 500;
+				ViewData["ErrorCode"] = 500;
+				ViewData["Message"] = "Произошла ошибка с доступом к серверу. Если проблема сохранится спустя некоторое время, то обратитесь в техническую поддержку";
+				return View("UserError");
+			}
 		}
 		[Authorize]
 		public async Task<IActionResult> GetNextPosts(Guid lastPostId)
 		{
-			Guid userId = Guid.Parse(HttpContext.User.FindFirstValue("CpcmUserId"));
-			List<PostModel> postsModel = new List<PostModel>();
-			var lastPost = await _context.CpcmPosts.Where(p => p.CpcmPostId == lastPostId).FirstOrDefaultAsync();
-			if (lastPost == null)
+			try
 			{
-				return StatusCode(404);
-			}
-
-			var groupIds = await _context.CpcmGroupfollowers
-				.Where(gf => gf.CpcmUserId == userId)
-				.Select(gf => gf.CpcmGroupId).ToListAsync();
-
-			// Получаем всех друзей пользователя
-			var friendIds = await _context.CpcmUserfriends
-				.Where(f => f.CmcpUserId == userId || f.CmcpFriendId == userId)
-				.Select(f => f.CmcpUserId == userId ? f.CmcpFriendId : f.CmcpUserId).ToListAsync();
-
-			// Получаем всех пользователей, на которых подписан пользователь
-			var followerIds = await _context.CpcmUserfollowers
-				.Where(f => f.CpcmUserId == userId)
-				.Select(f => f.CpcmFollowersId).ToListAsync();
-
-			// Объединяем все ID в одну коллекцию
-			var allUserIds = friendIds.Union(followerIds).ToList();
-
-			// Получаем все посты этих групп и пользователей
-			var posts = await _context.CpcmPosts
-				.Where(p => ((p.CpcmGroupId.HasValue && groupIds.Contains(p.CpcmGroupId.Value)) ||
-							(p.CpcmUserId.HasValue && allUserIds.Contains(p.CpcmUserId.Value))) &&
-							p.CpcmPostPublishedDate < lastPost.CpcmPostPublishedDate)
-				.Include(p => p.CpcmImages)
-				//.Include(p => p.CpcmPostFatherNavigation)
-				.OrderByDescending(c => c.CpcmPostPublishedDate)
-				.Take(10)
-				.ToListAsync();
-			foreach (var post in posts)
-			{
-				if (post.CpcmPostFather != null)
+				Guid userId = Guid.Parse(HttpContext.User.FindFirstValue("CpcmUserId"));
+				List<PostModel> postsModel = new List<PostModel>();
+				var lastPost = await _context.CpcmPosts.Where(p => p.CpcmPostId == lastPostId).FirstOrDefaultAsync();
+				if (lastPost == null)
 				{
-					post.CpcmPostFatherNavigation = await GetFatherPostReccurent(post);
+					return StatusCode(404);
 				}
-				CpcmUser? userOwner = await _context.CpcmUsers.Where(u => u.CpcmUserId == post.CpcmUserId).FirstOrDefaultAsync();
-				CpcmGroup? groupOwner = await _context.CpcmGroups.Where(u => u.CpcmGroupId == post.CpcmGroupId).FirstOrDefaultAsync();
-				long likes = await _context.Database.ExecuteSqlInterpolatedAsync($@"SELECT COUNT(*) FROM CPCM_POSTLIKES WHERE CPCM_PostID = '{post.CpcmGroupId}'");
-				long reposts = await _context.Database.ExecuteSqlInterpolatedAsync($@"SELECT COUNT(*) FROM CPCM_POSTREPOSTS WHERE CPCM_PostID = '{post.CpcmGroupId}'");
 
-				PostModel postModel = new() { Post = post, UserOwner = userOwner, GroupOwner = groupOwner, LikesCount = likes, RepostsCount = reposts };
-				postsModel.Add(postModel);
+				var groupIds = await _context.CpcmGroupfollowers
+					.Where(gf => gf.CpcmUserId == userId)
+					.Select(gf => gf.CpcmGroupId).ToListAsync();
+
+				// Получаем всех друзей пользователя
+				var friendIds = await _context.CpcmUserfriends
+					.Where(f => f.CmcpUserId == userId || f.CmcpFriendId == userId)
+					.Select(f => f.CmcpUserId == userId ? f.CmcpFriendId : f.CmcpUserId).ToListAsync();
+
+				// Получаем всех пользователей, на которых подписан пользователь
+				var followerIds = await _context.CpcmUserfollowers
+					.Where(f => f.CpcmUserId == userId)
+					.Select(f => f.CpcmFollowersId).ToListAsync();
+
+				// Объединяем все ID в одну коллекцию
+				var allUserIds = friendIds.Union(followerIds).ToList();
+
+				// Получаем все посты этих групп и пользователей
+				var posts = await _context.CpcmPosts
+					.Where(p => ((p.CpcmGroupId.HasValue && groupIds.Contains(p.CpcmGroupId.Value)) ||
+								(p.CpcmUserId.HasValue && allUserIds.Contains(p.CpcmUserId.Value))) &&
+								p.CpcmPostPublishedDate < lastPost.CpcmPostPublishedDate)
+					.Include(p => p.CpcmImages)
+					//.Include(p => p.CpcmPostFatherNavigation)
+					.OrderByDescending(c => c.CpcmPostPublishedDate)
+					.Take(10)
+					.ToListAsync();
+				foreach (var post in posts)
+				{
+					if (post.CpcmPostFather != null)
+					{
+						post.CpcmPostFatherNavigation = await GetFatherPostReccurent(post);
+					}
+					CpcmUser? userOwner = await _context.CpcmUsers.Where(u => u.CpcmUserId == post.CpcmUserId).FirstOrDefaultAsync();
+					CpcmGroup? groupOwner = await _context.CpcmGroups.Where(u => u.CpcmGroupId == post.CpcmGroupId).FirstOrDefaultAsync();
+					long likes = await _context.Database.ExecuteSqlInterpolatedAsync($@"SELECT COUNT(*) FROM CPCM_POSTLIKES WHERE CPCM_PostID = '{post.CpcmGroupId}'");
+					long reposts = await _context.Database.ExecuteSqlInterpolatedAsync($@"SELECT COUNT(*) FROM CPCM_POSTREPOSTS WHERE CPCM_PostID = '{post.CpcmGroupId}'");
+
+					PostModel postModel = new() { Post = post, UserOwner = userOwner, GroupOwner = groupOwner, LikesCount = likes, RepostsCount = reposts };
+					postsModel.Add(postModel);
+				}
+
+				return Json(postsModel);
 			}
-
-			return Json(postsModel);
+			catch (DbException)
+			{
+				return StatusCode(500);
+			}
 		}
 
 		private async Task<CpcmPost?> GetFatherPostReccurent(CpcmPost cpcmPostFatherNavigation)
