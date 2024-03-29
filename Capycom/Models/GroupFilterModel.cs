5eca61141c76287f70e474b488380c2cf3bb57c0
Phalaphun <phalaphun@capybara.su>
2024-03-29 22:38:39 +1000

    Переработаны методы по поиску людей подписоты и групп

---
+++ Capycom/Models/GroupFilterModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Capycom.Models
+{
+	public class GroupFilterModel
+	{
+		public Guid? UserId { get; set; }
+		public string? NickName { get; set; }
+		public string? Name { get; set; }
+		public int? SubjectID { get; set; }
+		public Guid? CityId { get; set; }
+				
+		public Guid? lastId { get; set; }
+
+	}
+}
