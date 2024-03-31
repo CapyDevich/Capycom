3d79ef84e2c7e5897b6ea61363a691bc7cf6f174
Phalaphun <phalaphun@capybara.su>
2024-03-31 14:18:49 +1000

    начата разработка

---
+++ Capycom/Controllers/GroupController.cs
@@ -0,0 +1,24 @@
+﻿using Capycom.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace Capycom.Controllers
+{
+	public class GroupController : Controller
+	{
+		private readonly CapycomContext _context;
+		private readonly MyConfig _config;
+		private readonly ILogger<GroupController> _logger;
+
+		public GroupController(ILogger<GroupController> logger, CapycomContext context, IOptions<MyConfig> config)
+		{
+			_context = context;
+			_config = config.Value;
+			_logger = logger;
+		}
+		public async Task<IActionResult> Index(GroupFilterModel filter)
+		{
+			return View();
+		}
+	}
+}
