1c9d9d2adce755a325af683c4a26237f48f1e647
Phalaphun <phalaphun@capybara.su>
2024-03-31 22:24:59 +1000

    Интегрирован метод на редактирования поста

---
+++ Capycom/Models/GroupPostEditModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Capycom.Models
+{
+	public class GroupPostEditModel
+	{
+		public Guid? Id { get; set; }
+		public Guid? GroupId { get; set; }
+
+		public string? Text { get; set; }
+
+		public Guid? PostFatherId { get; set; }
+
+		public List<Guid>? FilesToDelete { get; set; } = new List<Guid>();
+
+		[MaxFileCount(4)]
+		public List<IFormFile>? NewFiles { get; set; } = new List<IFormFile>();
+
+		public ICollection<CpcmImage>? CpcmImages { get; set; }
+
+		public DateTime? NewPublishDate { get; set; }
+	}
+}
