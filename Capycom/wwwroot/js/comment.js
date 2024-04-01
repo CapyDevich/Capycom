9aaef1ae01410bc91dd7d7da4b6e51ced2919e10
Sleepy <sleepy@capybara.su>
2024-04-01 19:38:26 +1000

    Добавлено поле для ввода комментария

---
+++ Capycom/wwwroot/js/comment.js
@@ -0,0 +1,11 @@
+﻿const textarea = document.getElementById('commentText');
+textarea.addEventListener('input', function () {
+
+	if (this.scrollHeight > 200)
+		this.style.overflow = 'scroll';
+	else {
+		this.style.overflow = 'hidden';
+		this.style.height = 'auto';
+		this.style.height = (this.scrollHeight) + 'px';
+	}
+});
