2dfaf7fb3166129d9f3c22ec24a2db89550dc62d
Sleepy <sleepy@capybara.su>
2024-04-01 20:49:47 +1000

    Ошибка при загрузке более двух файлов в комментарий

--- Capycom/wwwroot/js/comment.js
+++ Capycom/wwwroot/js/comment.js
@@ -3,21 +3,36 @@ const fileList = document.querySelector('.file-list');
 
 fileInput.addEventListener('change', function () {
     fileList.innerHTML = '';
-    Array.from(fileInput.files).forEach(file => {
+    if (fileInput.files.length <= 2) {
+        Array.from(fileInput.files).forEach(file => {
+            const listItem = document.createElement('div');
+            listItem.classList.add('d-flex', 'justify-content-between', 'align-items-center');
+            const fileName = document.createTextNode(file.name);
+            listItem.appendChild(fileName);
+            const cancelBtn = document.createElement('button');
+            cancelBtn.classList.add('btn', 'btn-outline-danger', 'btn-sm', 'border-0');
+            cancelBtn.innerHTML = '&times;';
+            cancelBtn.addEventListener('click', function () {
+                listItem.remove();
+                fileInput.value = null;
+            });
+            listItem.appendChild(cancelBtn);
+            fileList.appendChild(listItem);
+        });
+    }
+    else {
         const listItem = document.createElement('div');
-        listItem.classList.add('d-flex', 'justify-content-between', 'align-items-center');
-        const fileName = document.createTextNode(file.name);
-        listItem.appendChild(fileName);
+        listItem.classList.add('d-flex', 'justify-content-between', 'text-danger', 'ps-1', 'mb-1', 'rounded');
+        listItem.innerHTML = "Можно загрузить до 2 файлов"
         const cancelBtn = document.createElement('button');
         cancelBtn.classList.add('btn', 'btn-outline-danger', 'btn-sm', 'border-0');
         cancelBtn.innerHTML = '&times;';
         cancelBtn.addEventListener('click', function () {
             listItem.remove();
-            fileInput.value = null;
         });
-        listItem.appendChild(cancelBtn);
         fileList.appendChild(listItem);
-    });
+        listItem.appendChild(cancelBtn);
+    }
 });
 
 const textarea = document.getElementById('commentText');
