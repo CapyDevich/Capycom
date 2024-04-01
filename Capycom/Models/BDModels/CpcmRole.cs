4b280e07075e38c25d1e6aff4067a8a7c897c630
Phalaphun <phalaphun@capybara.su>
2024-04-01 18:25:11 +1000

    Админский контроллер по работе с ролями готов. нет только вьюшки под редактированиероли пользователя

--- Capycom/Models/BDModels/CpcmRole.cs
+++ Capycom/Models/BDModels/CpcmRole.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 
 namespace Capycom;
 
 public partial class CpcmRole
 {
+    [ValidateNever]
     public int CpcmRoleId { get; set; }
 
     public string CpcmRoleName { get; set; } = null!;
