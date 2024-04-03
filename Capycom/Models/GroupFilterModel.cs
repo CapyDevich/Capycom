94e198f1e5d3728b4074553da8764e7f587a08f8
Phalaphun <phalaphun@capybara.su>
2024-04-03 12:26:12 +1000

    Валидирую ники групп

--- Capycom/Models/GroupFilterModel.cs
+++ Capycom/Models/GroupFilterModel.cs
@@ -1,8 +1,11 @@
-﻿namespace Capycom.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Capycom.Models
 {
 	public class GroupFilterModel
 	{
 		public Guid? UserId { get; set; }
+		[RegularExpression(@"^[a-zA-Z0-9_\-]*$", ErrorMessage = "Nickname может содержать только буквы латиницы, цифры, подчеркивания и дефисы.")]
 		public string? NickName { get; set; }
 		public Guid? GroupId { get; set; }
 		public string? Name { get; set; }
