d63ee8eb491496c3bb7da255ddbb2c730ed1d37c
Sleepy <sleepy@capybara.su>
2024-04-04 22:01:08 +1000

    Добавлен глобальный cookie на часовой пояс

--- Capycom/wwwroot/js/site.js
+++ Capycom/wwwroot/js/site.js
@@ -2,3 +2,5 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+let offset = new Date().getTimezoneOffset();
+document.cookie = `TimeZone=${offset}; path=/`;
