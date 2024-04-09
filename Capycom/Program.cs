d842454b190656e0b91669e6de1ce3d48023e1b4
Phalaphun <phalaphun@capybara.su>
2024-04-09 23:29:20 +1000

    Добавлен отлов непойманных ошибок

--- Capycom/Program.cs
+++ Capycom/Program.cs
@@ -69,9 +69,9 @@ namespace Capycom
 			//}));
 
 			var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+			app.UseSerilogRequestLogging();
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
