1a4ecc1ade1bce5829c46db853afc6eab3bab8cd
Phalaphun <phalaphun@capybara.su>
2024-04-13 20:16:16 +1000

    Добавлены контроллеры и модели для отображения неизвестных ошибок

---
+++ Capycom/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿using Capycom.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Capycom.Controllers
+{
+    public class ErrorController : Controller
+	{
+
+		public IActionResult Error(ErrorModel errorModel)
+		{
+			if (errorModel != null)
+			{
+				Response.StatusCode = errorModel.StatusCode;
+				return View("UserError",errorModel);
+			}
+			Response.StatusCode = 500;
+			return View("UserError",errorModel);
+		}
+	}
+}
