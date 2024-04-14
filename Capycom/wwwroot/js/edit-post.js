fcc42b331eefadca5ee98f51fc6a1ee7ccf502b1
Sleepy <sleepy@capybara.su>
2024-04-14 17:35:29 +1000

    Форма изменения поста: логика удаления и добавления изображений, изменение текста и даты, если пост запланирован

--- Capycom/wwwroot/js/edit-post.js
+++ Capycom/wwwroot/js/edit-post.js
@@ -4,7 +4,7 @@ const fileList = document.querySelector('.file-list');
 
 fileInput.addEventListener('change', function () {
     fileList.innerHTML = '';
-    if (fileInput.files.length <= 4) {
+    if (fileInput.files.length + $('.existing-img').length - $('.delete-img').length <= 4) {
         Array.from(fileInput.files).forEach(file => {
             const listItem = document.createElement('div');
             listItem.classList.add('d-flex', 'justify-content-between', 'align-items-center');
@@ -24,7 +24,7 @@ fileInput.addEventListener('change', function () {
     else {
         const listItem = document.createElement('div');
         listItem.classList.add('d-flex', 'justify-content-between', 'text-danger', 'ps-1', 'mb-1', 'rounded');
-        listItem.innerHTML = "Можно загрузить до 2 файлов"
+        listItem.innerHTML = "Можно загрузить до 4 файлов"
         const cancelBtn = document.createElement('button');
         cancelBtn.classList.add('btn', 'btn-outline-danger', 'btn-sm', 'border-0');
         cancelBtn.innerHTML = '&times;';
@@ -47,3 +47,29 @@ textarea.addEventListener('input', function () {
         this.style.height = (this.scrollHeight) + 'px';
     }
 });
+
+function darkenImage(image) {
+    const img = $(image);
+    if (!img.hasClass('darken-image'))
+        $("#EditPost").append(`<input type="hidden" class="delete-img" name="FilesToDelete[]" id="to-send-${image.id}" value="${image.id}">`);
+    else {
+        $(`#to-send-${image.id}`).remove();
+        if (fileInput.files.length + $('.existing-img').length - $('.delete-img').length > 4) {
+            fileList.innerHTML = '';
+            fileInput.value = null;
+
+            const listItem = document.createElement('div');
+            listItem.classList.add('d-flex', 'justify-content-between', 'text-danger', 'ps-1', 'mb-1', 'rounded');
+            listItem.innerHTML = "Можно загрузить до 4 файлов";
+            const cancelBtn = document.createElement('button');
+            cancelBtn.classList.add('btn', 'btn-outline-danger', 'btn-sm', 'border-0');
+            cancelBtn.innerHTML = '&times;';
+            cancelBtn.addEventListener('click', function () {
+                listItem.remove();
+            });
+            fileList.appendChild(listItem);
+            listItem.appendChild(cancelBtn);
+        }
+    }
+    img.toggleClass('darken-image');
+}
