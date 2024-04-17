7464682cca5598444152070803aba97b54b54c9c
Sleepy <sleepy@capybara.su>
2024-04-17 22:59:47 +1000

    Кнопка "загрузить ещё" теперь работает, подгружает следующую пачку постов и вставляет их на страницу

---
+++ Capycom/wwwroot/js/load-more.js
@@ -0,0 +1,28 @@
+﻿'use strict'
+function loadNews(button) {
+    let dataToSend = {
+        lastPostId: $(`.PostId`).last()[0].innerText
+    };
+    $.ajax({
+        url: '/News/GetNextPosts',
+        type: 'POST',
+        data: dataToSend,
+        success: function (response) {
+            let postBody = document.getElementsByClassName('col-lg-8')[0];
+            if (response != '') {
+                postBody.innerHTML += response;
+            }
+            else {
+                button.remove();
+                postBody.innerHTML += '<p class="text-center">У наших капибар закончились новости!</p>'
+            } 
+        },
+        error: function (obj) {
+            if (obj.status == 401)
+                window.location.replace("/UserLogIn");
+            else
+                alert(`:(\nСтатус: ${obj.status}`);
+        }
+    });
+
+}
