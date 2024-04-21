92d0e26a249fab7b29f1df7e7a8f56ac3ecdf437
Sleepy <sleepy@capybara.su>
2024-04-22 08:50:48 +1000

    Фикс вечной загрузки при попытке подгрузить ещё постов в профиле пользователя, у которого нет постов

--- Capycom/wwwroot/js/load-more.js
+++ Capycom/wwwroot/js/load-more.js
@@ -35,35 +35,40 @@ function loadNews(button) {
     }
 }
 function loadProfilePost(button) {
-    button.innerHTML = '<img src="../images/loading.svg" />';
-    let dataToSend = {
-        userId: $(`#userID`)[0].innerText,
-        lastPostId: $(`.PostId`).last()[0].innerText
-    };
-    $.ajax({
-        url: '/User/GetNextPosts',
-        type: 'POST',
-        data: dataToSend,
-        success: function (response) {
-            let feedBody = document.getElementsByClassName('feed')[0];
-            if (response != '') {
-                feedBody.innerHTML += response;
+    if ($(`.PostId`).length != 0) {
+        button.innerHTML = '<img src="../images/loading.svg" />';
+        let dataToSend = {
+            userId: $(`#userID`)[0].innerText,
+            lastPostId: $(`.PostId`).last()[0].innerText
+        };
+        $.ajax({
+            url: '/User/GetNextPosts',
+            type: 'POST',
+            data: dataToSend,
+            success: function (response) {
+                let feedBody = document.getElementsByClassName('feed')[0];
+                if (response != '') {
+                    feedBody.innerHTML += response;
+                    button.innerHTML = 'Загрузить ещё';
+                }
+                else {
+                    button.remove();
+                    feedBody.innerHTML += '<p class="text-center">У наших капибар закончились новости!</p>'
+                }
+            },
+            error: function (obj) {
+                if (obj.status == 401)
+                    window.location.replace("/UserLogIn");
+                else
+                    alert(`:(\nСтатус: ${obj.status}`);
                 button.innerHTML = 'Загрузить ещё';
             }
-            else {
-                button.remove();
-                feedBody.innerHTML += '<p class="text-center">У наших капибар закончились новости!</p>'
-            }
-        },
-        error: function (obj) {
-            if (obj.status == 401)
-                window.location.replace("/UserLogIn");
-            else
-                alert(`:(\nСтатус: ${obj.status}`);
-            button.innerHTML = 'Загрузить ещё';
-        }
-    });
-
+        });
+    }
+    else {
+        button.remove();
+        document.getElementsByClassName('feed')[0].innerHTML += '<p class="text-center">У наших капибар нет новостей!</p>'
+    }
 }
 function loadFollowers(button) {
     if ($('.follower').length > 0) {
