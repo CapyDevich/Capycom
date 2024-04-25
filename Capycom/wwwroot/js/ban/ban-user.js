0b036b3304768e037f1fa067b4d3f96ad26535e8
Sleepy <sleepy@capybara.su>
2024-04-25 23:18:47 +1000

    Переписана система банов

---
+++ Capycom/wwwroot/js/ban/ban-user.js
@@ -0,0 +1,25 @@
+﻿'use strict'
+function banUser(url, banID) {
+    let dataToSend = {
+        id: banID
+    };
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: dataToSend,
+        success: function (response) {
+            if (response['status']) {
+                window.location.reload();
+            }
+            else {
+                alert('Забанить не вышло :(');
+            }
+        },
+        error: function (obj) {
+            if (obj.status == 401)
+                window.location.replace("/UserLogIn");
+            else
+                alert(`Забанить не вышло, что-то пошло не так :(\nСтатус: ${obj.status}`);
+        }
+    });
+}
