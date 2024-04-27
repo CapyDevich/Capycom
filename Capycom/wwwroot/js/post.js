c81a110c7537705c90d5b060b7b9ab2d0b8d55e3
Sleepy <sleepy@capybara.su>
2024-04-27 20:58:03 +1000

    Добавлено ограничении на ввод до 300 символов в репост, а также запрет на репосты без текста

--- Capycom/wwwroot/js/post.js
+++ Capycom/wwwroot/js/post.js
@@ -56,32 +56,53 @@ function repost(button, postId) {
     $('#repostSend').click(function () {
         let inputText = $('#repostInput').val();
 
-        let dataToSend = {
-            PostFatherId: postId,
-            Text: inputText
-        };
-        $.ajax({
-            url: '/User/CreatePostP',
-            type: 'POST',
-            data: dataToSend,
-            success: function (response) {
-                if (response['status']) {
-                    repostCountElement.innerText = Number(repostCountElement.innerText) + 1;
-                    renderPostButtons();
-                    postID = "";
-                    inputText = "";
-                    document.getElementById('repostInput').value = "";
-                }
-                else {
-                    console.log("response was not successful");
-                }
+        if (inputText != '') {
+            if (inputText.length <= 300) {
+                let dataToSend = {
+                    PostFatherId: postId,
+                    Text: inputText
+                };
+                $.ajax({
+                    url: '/User/CreatePostP',
+                    type: 'POST',
+                    data: dataToSend,
+                    success: function (response) {
+                        if (response['status']) {
+                            document.getElementById('repostAlert').style.display = 'none';
+                            repostCountElement.innerText = Number(repostCountElement.innerText) + 1;
+                            renderPostButtons();
+                            postID = "";
+                            inputText = "";
+                            document.getElementById('repostInput').value = "";
+                            bootstrap.Modal.getInstance(document.getElementById('repostModal')).hide();
+                        }
+                        else {
+                            let alertUserField = document.getElementById('repostAlert');
+                            alertUserField.innerHTML = 'Не получилось создать репост.';
+                            alertUserField.style.display = '';
+                        }
 
-            },
-            error: function (obj) {
-                if (obj.status == 401)
-                    window.location.replace("/UserLogIn");
+                    },
+                    error: function (obj) {
+                        if (obj.status == 401)
+                            window.location.replace("/UserLogIn");
+                        else {
+                            alert('Случилось, что случилось...\nСтатус ответа: ' + obj.status);
+                        }
+                    }
+                });
             }
-        });
+            else {
+                let alertUserField = document.getElementById('repostAlert');
+                alertUserField.innerHTML = 'Репост должен содержать не более 300 символов.<br/>Кол-во символов у вас: ' + inputText.length;
+                alertUserField.style.display = '';
+            }
+        }
+        else {
+            let alertUserField = document.getElementById('repostAlert');
+            alertUserField.innerHTML = 'Нельзя создать репост без текста.';
+            alertUserField.style.display = '';
+        }
     });
 }
 
@@ -283,5 +304,5 @@ function deletePost() {
                 lastIDToDelete = ''
             }
         });
-    
+
 }
